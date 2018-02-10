08f9568b0639622f9fd28e7bf94acaf98a49ee3a
noahgol <noah_g@verizon.net>
2018-02-10 01:32:54 -0500

    trigrams work, working on nnlm

---
+++ hw2/helpers.py
@@ -0,0 +1,62 @@
+import torchtext
+from torchtext.vocab import Vectors
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import itertools as it
+
+
+class LangEvaluator(object):
+    def __init__(self, model, TEXT, **kwargs):
+        self._TEXT = TEXT
+        self.model = model
+        self.eval_metric = kwargs.get('evalmetric', 'perplexity')
+        
+    def evaluate(self, test_iter, num_iter=None):
+        sum_nll = 0
+        cnt_nll = 0
+        for i,batch in enumerate(test_iter):
+            if i % 100 == 0:
+                print('Iteration %d' % i)
+            # Model output: [batch_size, sent_len, size_vocab]; these
+            # aren't actually probabilities if the model is a Trigram,
+            # but this doesn't matter.
+            batch_transpose = torch.t(batch.text).contiguous() # [batch_size, sent_len]
+            log_probs = self.model(batch_transpose)
+            cnt_nll += batch_transpose.size()[0] * batch_transpose.size()[1]
+            sum_nll += LangTrainer.loss_nll(batch_transpose.data,
+                                            log_probs, mode='sum')
+            if not num_iter is None and i > num_iter:
+                break
+            
+        return np.exp(sum_nll / cnt_nll)
+
+
+class LangTrainer(object):
+    def __init__(self, TEXT, model, **kwargs):
+        self._TEXT = TEXT
+        self._model = model
+    
+    # Here batch is output from a RNN/NNLM/Trigram model:
+    # [..., size_vocab], and output are the real words: [...]
+    @staticmethod
+    def loss_nll(batch, output, mode='mean'):
+        # [batch_size * sent_len, size_vocab]
+        vocab_len = output.size()[-1]
+        output = output.view(-1, vocab_len)
+        # [batch_size * sent_len]
+        batch = batch.view(-1, 1)
+        batch_probs = -1 * torch.gather(output, 1, batch)
+        if mode == 'mean':
+            return torch.mean(batch_probs)
+        else:
+            return torch.sum(batch_probs)
+        return
+    
+    @staticmethod
+    def loss_perplexity(*args):
+        return torch.exp(LangTrainer.loss_nll(*args))
+        
--- hw2/models.py
+++ hw2/models.py
@@ -1,1 +1,85 @@
-# TODO
+import torchtext
+from torchtext.vocab import Vectors
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import itertools as it
+
+class Trigram(nn.Module):
+    def __init__(self, TEXT, **kwargs):
+        super(Trigram, self).__init__()
+        self._TEXT = TEXT
+        self._text_vocab_len = len(TEXT.vocab)
+        
+        # Use dictionaries since we don't want to have to 
+        # store the vast majority of bi/tri gram counts 
+        # which are 0.
+        self.cnts = [dict(), dict(), dict()]
+        
+    def set_alpha(self, *args):
+        self.alphas = list(args)
+        if len(self.alphas) < 3:
+            assert len(self.alphas) == 2
+            self.alphas.append(1 - sum(self.alphas))
+            
+    def normalize_cnts(self):
+        for n in range(3):
+            for k in self.cnts[n]:
+                self.cnts[n][k] = self.cnts[n][k] / torch.sum(self.cnts[n][k])
+        
+    def train_counts(self, train_iter, num_iter=None):
+        if num_iter is None:
+            num_iter = len(train_iter)
+        train_iter = iter(train_iter)
+        for i in range(num_iter):
+            batch = next(train_iter)
+            if i % 1000 == 0:
+                print('Iteration %d' % i)
+            self.update_trigram_cnts(torch.t(batch.text).data)
+        self.normalize_cnts()
+            
+    # Returns a torch tensor of size [size_batch, sentence_len,
+    # size_vocab]; this returns the probability vectors for each of
+    # the words This is quite slow, but the trigram model isn't a
+    # bottleneck so there's no point in making it faster
+    def forward(self, batch):
+        ret_arr = torch.zeros(batch.size()[0], batch.size()[1], 
+                              self._text_vocab_len)
+        for i in range(batch.size()[0]):
+            for n in range(0,3):
+                for j in range(batch.size()[1]):
+                    key = () if max(0, j-n) == j else \
+                          tuple(batch.data[i, max(0, j-n):j])
+                    if key in self.cnts[n]:
+                        ret_arr[i,j,:] += self.alphas[n] * self.cnts[n][key]
+                    else:
+                        # This is equivalent to adding NB-type
+                        # smoothing, but more efficient memory-wise
+                        ret_arr[i,j,:] += self.alphas[n] * torch.ones(
+                            self._text_vocab_len) / self._text_vocab_len
+        # Use log probabilities to make this work with LangEvaluator
+        # case_sums = torch.sum(ret_arr, dim=2, keepdim=True)
+        # print(torch.sum(case_sums, dim=2))
+        # Using broadcasting
+        return torch.log(ret_arr)
+                
+    # Batch is an torch tensor of size [batch_size, bptt_len]
+    def update_trigram_cnts(self, batch):
+        # We don't glue rows together since they may be shuffled (this
+        # is all kind of silly since ideally we'd just do this in one
+        # big 'sentence', but perhaps we want a 'fair comparison'...)
+        for j in range(batch.shape[0]):
+            for n in range(0,3):
+                for k in range(batch.shape[1] - n):
+                    dict_key = () if k == k+n else tuple(batch[j, k:k+n])
+                    if not dict_key in self.cnts[n]:
+                        # We never want to return 0 probability (or
+                        # else PPL = infty), so make sure we've got
+                        # mass everywhere
+                        self.cnts[n][dict_key] = torch.zeros(self._text_vocab_len) if \
+                                                 n > 0 else torch.ones(self._text_vocab_len)
+                    # Here's where we increment the ocunt
+                    self.cnts[n][dict_key][batch[j, k+n]] += 1    
