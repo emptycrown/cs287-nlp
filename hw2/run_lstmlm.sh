7fd93e46cd7e219ec5070aad39c9ddead6a87de3
emptycrown <jessetanzhang@college.harvard.edu>
2018-02-12 17:51:02 -0500

    pp

--- hw2/run_lstmlm.sh
+++ hw2/run_lstmlm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 main.py --network lstmlm --tt_num_iter=100000 --early_stop --bptt_len=35 --batch_sz=20 --t_lrn_rate=1.0 --t_lrn_decay=invlin --t_lrn_decay_rate=1.0 --m_word_features=100 --t_optimizer=sgd --t_clip_norm=5 --m_num_layers=2 --m_word_features=100 --m_dropout=0.5 --m_hidden_size=650
+python3 main.py --network lstmlm --tt_num_iter=100000 --early_stop --bptt_len=36 --batch_sz=128 --t_lrn_rate=1.0 --t_lrn_decay=invlin --t_lrn_decay_rate=0.1 --m_word_features=100 --t_optimizer=sgd --t_clip_norm=5 --m_num_layers=2 --m_word_features=100 --m_dropout=0.5 --m_hidden_size=512 --tt_produce_predictions
 
 
 declare -a LR_BASES=(0.0005 0.001 0.002)
