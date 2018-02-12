9d27668d13e108f1376155516b1e8120bde932f1
Ubuntu <jesse@jtzhang.khdp0ilepjve1iawpxnhuxirob.xx.internal.cloudapp.net>
2018-02-12 22:07:07 +0000

    pp

--- hw2/run_nnlm.sh
+++ hw2/run_nnlm.sh
@@ -10,9 +10,9 @@
 # python3 main.py --tt_num_iter=100000 --early_stop --batch_sz=10 --bptt_len=64 --t_lrn_rate=0.001 --t_lrn_decay=invlin --t_lrn_decay_rate=0.1 --m_word_features=100 --t_optimizer=adam --m_kern_size_inner=4
 
 # Scores 171.00 after 34 epochs (dropout = 0.25)
-# python3 main.py --tt_num_iter=100000 --early_stop --batch_sz=1 --bptt_len=1000 --t_lrn_rate=0.001 --t_lrn_decay=invlin --t_lrn_decay_rate=0.1 --m_word_features=100 --t_optimizer=adam --m_kern_size_inner=4 --m_dropout=0.25
+python3 main.py --tt_num_iter=100000 --early_stop --batch_sz=1 --bptt_len=1000 --t_lrn_rate=0.001 --t_lrn_decay=invlin --t_lrn_decay_rate=0.1 --m_word_features=100 --t_optimizer=adam --m_kern_size_inner=4 --m_dropout=0.25 --produce_predictions
 
-python3 main.py --network lstmlm --tt_num_iter=100000 --early_stop --bptt_len=36 --batch_sz=128 --t_lrn_rate=1.0 --t_lrn_decay=invlin --t_lrn_decay_rate=0.1 --m_word_features=100 --t_optimizer=sgd --t_clip_norm=5 --m_num_layers=2 --m_word_features=100 --m_dropout=0.5 --m_hidden_size=512
+# python3 main.py --network lstmlm --tt_num_iter=100000 --early_stop --bptt_len=36 --batch_sz=128 --t_lrn_rate=1.0 --t_lrn_decay=invlin --t_lrn_decay_rate=0.1 --m_word_features=100 --t_optimizer=sgd --t_clip_norm=5 --m_num_layers=2 --m_word_features=100 --m_dropout=0.5 --m_hidden_size=512
 
 
 declare -a LR_BASES=(0.0005 0.001 0.002)
